32552bfa9c0facd49eb7de91cf1da276e2f958c3
Rahmat Hidayat <yayatbx@gmail.com>
2024-03-26 03:25:35 +0800

    add Sell Page Components

--- app/common/sell/page.tsx
+++ app/common/sell/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import TablesSell from "./components/TablesSell";
+import { Button } from "@/components/ui/button";
 
 export default function Sell() {
   return (
-    <div className="container py-6">Sell Page</div>
-  )
+    <div className="container py-8">
+      <div className="flex justify-between pb-6 items-center">
+        <h1 className="text-3xl font-semibold">On Stock !</h1>
+        <Button>Add Data</Button>
+      </div>
+      <div>
+        <TablesSell />
+      </div>
+    </div>
+  );
 }
