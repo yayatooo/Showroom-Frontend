61c688b55f06b540249fa2b1e77a601088d44f08
Rahmat Hidayat <yayatbx@gmail.com>
2024-04-17 21:25:51 +0800

    pemanasan setelah libur lebaran

--- app/common/dashboard/page.tsx
+++ app/common/dashboard/page.tsx
@@ -1,16 +1,22 @@
 import { CardDashboard } from "@/app/ui/CardDashboard";
 import { TableDemo } from "@/app/ui/Table";
+import { TbCurrencyDollar,TbMotorbike  } from "react-icons/tb";
+import { CgNotes } from "react-icons/cg";
+import { FormatRupiah } from "@arismun/format-rupiah";
 
 import React from "react";
 
 export default function Dashboard() {
+
+  const valueOfKeutungan = <FormatRupiah value={85000000} />
+
   return (
     <>
       <h1 className="text-3xl font-semibold">Dashboard</h1>
       <div className="flex gap-4">
-        <CardDashboard />
-        <CardDashboard />
-        <CardDashboard />
+        <CardDashboard title="Keuntungan" capitals={valueOfKeutungan} logo={<TbCurrencyDollar />} />
+        <CardDashboard title="Jumlah Motor" capitals={10} logo={<TbMotorbike />} />
+        <CardDashboard title="Motor Laku" capitals={5} logo={<CgNotes />} />
       </div>
       <div className="py-3">
       <TableDemo />
--- app/ui/CardDashboard.tsx
+++ app/ui/CardDashboard.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { TbCurrencyDollar } from "react-icons/tb";
+import { TbCurrencyDollar, } from "react-icons/tb";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Image from "next/image";
+
 
 interface CardProps {
-  children: React.ReactNode;
+  title: string;
+  capitals: any;
+  logo: any// Specify the type as IconType
 }
 
-export const CardDashboard = () => {
+export const CardDashboard: React.FC<CardProps> = ({ title, capitals, logo }) => {
   return (
     <Card className="w-full my-4 p-6">
       <CardContent className="p-0 flex justify-between">
-        <p className="font-semibold text-sm">Keuntungan</p>
-        <p><TbCurrencyDollar /></p>
+        <p className="font-semibold text-sm">{title}</p>
+        {logo} 
       </CardContent>
       <CardHeader className="pt-3 px-0 pb-0">
-        <CardTitle>Rp. 58.000.000</CardTitle>
+        <CardTitle>{capitals}</CardTitle>
       </CardHeader>
     </Card>
   );
